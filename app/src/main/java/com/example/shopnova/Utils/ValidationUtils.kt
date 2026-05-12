9916fde5e4ac095e69aac702ffd07739937f6c29
LeonidasVS <villafrancosibrian@gmail.com>
2026-05-11 21:56:16 -0600

    Proyecto Finalizado

--- app/src/main/java/com/example/shopnova/Utils/ValidationUtils.kt
+++ app/src/main/java/com/example/shopnova/Utils/ValidationUtils.kt
@@ -5,7 +5,7 @@ object ValidationUtils {
         android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
 
     fun isValidPassword(password: String): Boolean =
-        password.length >= 5
+        password.length >= 6
 
     fun isNotEmpty(text: String): Boolean =
         text.trim().isNotEmpty()
@@ -17,5 +17,5 @@ object ValidationUtils {
         try { stock.toInt() >= 0 } catch (e: NumberFormatException) { false }
 
     fun isValidName(name: String): Boolean =
-        name.length <= 75
+        name.length <= 20
 }
